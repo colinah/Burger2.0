9d255e368319663fe806be67c3456acb1296a297
Colin Hale <halecolin1@gmail.com>
2019-10-18 12:16:15 -0600

    Adding shouldComponentUpdate to Modal

--- burger/src/components/Burger/OrderSummary/OrderSummary.js
+++ burger/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,29 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../../hoc/Aux'
 import Button from '../../UI/Button/Button'
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
-    .map(igKey => {
-        return <li key = {igKey}>
-                <span style= {{textTransform: 'capitalize'}}>{igKey}</span>
-                : {props.ingredients[igKey]}
-                </li>
-    })
-    
-    return (
-    <Aux>
-        <h3>Your Order</h3>
-        <p>A delicious burger with the folling ingredients:</p>
-        <ul>
-        {ingredientSummary}
-        </ul>
-        <p><strong>Price: ${props.price.toFixed(2)}</strong></p>
-        <p>Continue to Checkout?</p>
-        <Button btnType='Danger' clicked={props.cancel}>CANCEL</Button>
-        <Button btnType='Success' clicked={props.continue} >CONTINUE</Button>
-    </Aux>
-    )
+class OrderSummary extends Component {
+    render(){
+        const ingredientSummary = Object.keys(this.props.ingredients)
+        .map(igKey => {
+            return <li key = {igKey}>
+                    <span style= {{textTransform: 'capitalize'}}>{igKey}</span>
+                    : {this.props.ingredients[igKey]}
+                    </li>
+        })
+       return (
+        <Aux>
+            <h3>Your Order</h3>
+            <p>A delicious burger with the folling ingredients:</p>
+            <ul>
+            {ingredientSummary}
+            </ul>
+            <p><strong>Price: ${this.props.price.toFixed(2)}</strong></p>
+            <p>Continue to Checkout?</p>
+            <Button btnType='Danger' clicked={this.props.cancel}>CANCEL</Button>
+            <Button btnType='Success' clicked={this.props.continue} >CONTINUE</Button>
+        </Aux>
+       ) 
+    }
 }
 
-export default orderSummary;
+export default OrderSummary
