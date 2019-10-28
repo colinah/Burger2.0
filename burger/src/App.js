85511c180376b4806537f500eeff01327c7acfe9
Colin Hale <halecolin1@gmail.com>
2019-10-28 15:01:36 -0600

    adding Orders.js page

--- burger/src/App.js
+++ burger/src/App.js
@@ -3,7 +3,7 @@ import {Route,Switch} from 'react-router-dom';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
-
+import Orders from './containers/Orders/Orders';
 
 function App() {
   return (
@@ -11,7 +11,9 @@ function App() {
       <Layout>
         <Switch>
           <Route path="/checkout" component = {Checkout} />
+          <Route path="/orders" component={Orders} />
           <Route path="/" component={BurgerBuilder} />
+          
         </Switch>
       </Layout>
     </div>
