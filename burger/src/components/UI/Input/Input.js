296dfc9e78a12403c9eaac10f3f0935428e0827d
Colin Hale <halecolin1@gmail.com>
2019-10-29 11:17:01 -0600

    Setting up select element in Input.js

--- burger/src/components/UI/Input/Input.js
+++ burger/src/components/UI/Input/Input.js
@@ -8,8 +8,20 @@ const input = (props) => {
         case('input'):
         inputElement = <input className={classes.InputElement} {...props.elementConfig} value={props.value}/>
             break;
-        case('testarea'):
+        case('textarea'):
             inputElement = <textarea className={classes.InputElement} {...props.elementConfig} value={props.value}/>
+            break;
+        case('select'):
+            inputElement = <select 
+                            className={classes.InputElement}
+                            value={props.value}>
+                                {props.elementConfig.options.map(option => (
+                                    <option key={option.value}
+                                            value={option.value}>
+                                        {option.displayValue}
+                                    </option>
+                                ))}
+                            </select>
             break;
         default:
             inputElement = <input className={classes.InputElement} {...props.elementConfig} value={props.value}/>
