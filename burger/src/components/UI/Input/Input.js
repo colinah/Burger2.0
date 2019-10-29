13d2a00f753d05d5f41beac41bf717066cd83dcb
Colin Hale <halecolin1@gmail.com>
2019-10-29 12:18:52 -0600

    Adding onChange handlers to inputs in Input.js

--- burger/src/components/UI/Input/Input.js
+++ burger/src/components/UI/Input/Input.js
@@ -6,25 +6,37 @@ const input = (props) => {
 
     switch(props.elementType) {
         case('input'):
-        inputElement = <input className={classes.InputElement} {...props.elementConfig} value={props.value}/>
+        inputElement = <input 
+            onChange={props.change} 
+            className={classes.InputElement}
+            {...props.elementConfig}
+            value={props.value}/>
             break;
         case('textarea'):
-            inputElement = <textarea className={classes.InputElement} {...props.elementConfig} value={props.value}/>
+            inputElement = <textarea 
+                onChange={props.change} 
+                className={classes.InputElement}
+                {...props.elementConfig}
+                value={props.value}/>
             break;
         case('select'):
-            inputElement = <select 
-                            className={classes.InputElement}
-                            value={props.value}>
+            inputElement = <select onChange={props.change}
+                            className={classes.InputElement}>
                                 {props.elementConfig.options.map(option => (
-                                    <option key={option.value}
-                                            value={option.value}>
+                                    <option  
+                                        key={option.value}
+                                        value={option.value}>
                                         {option.displayValue}
                                     </option>
                                 ))}
                             </select>
             break;
         default:
-            inputElement = <input className={classes.InputElement} {...props.elementConfig} value={props.value}/>
+            inputElement = <input 
+                onChange = {props.change} 
+                className={classes.InputElement}
+                {...props.elementConfig}
+                value={props.value}/>
     }
     return(
         <div className={classes.Input} >
