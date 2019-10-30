b1724fda1f0946edfd9295c4c059ed24374911f6
Colin Hale <halecolin1@gmail.com>
2019-10-29 21:32:10 -0600

    Removing variable purchasable from state

--- burger/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -14,7 +14,6 @@ import * as actionTypes from '../../store/actions';
 
 class BurgerBuilder extends Component {
     state = {
-        purchasable: false,
         purchasing:  false,
         loading: false,
         error: false
@@ -60,7 +59,7 @@ class BurgerBuilder extends Component {
         .reduce((sum,el) => {
             return sum + el
         }, 0)
-        this.setState({purchasable:sum>0})
+        return sum>0
     }
 
     // addIngredientHandler = (type) => {
@@ -113,7 +112,7 @@ class BurgerBuilder extends Component {
                     ingredientRemoved={this.props.onIngredientRemoved}
                     disabled = {disabledInfo}
                     price = {this.props.totalPrice}
-                    purchasable = {this.state.purchasable}
+                    purchasable = {this.updatePrechaseState(this.props.ings)}
                     purchase = {this.purchaseHandler} 
                     />
                 </Aux>
