bff97dd03a1ba5fa970a8a9934f166a5fe3636c1
Colin Hale <halecolin1@gmail.com>
2019-11-15 11:03:06 -0700

    Using updateObject for object changes in Auth and ContactData

--- burger/src/containers/Auth/Auth.js
+++ burger/src/containers/Auth/Auth.js
@@ -6,7 +6,7 @@ import Button from '../../components/UI/Button/Button';
 import classes from './Auth.module.css';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import * as actions from '../../store/actions/index';
-
+import { updateObject } from '../../shared/utility';
 
 
 
@@ -79,16 +79,13 @@ state = {
     }
 
     inputChangeHandler = (event , controlName) => {
-        const updatedControls = {
-            ...this.state.controls,
-            [controlName]: {
-                ...this.state.controls[controlName],
+        const updatedControls = updateObject(this.state.controls,{
+            [controlName]: updateObject(this.state.controls[controlName],{
                 value: event.target.value,
                 valid: this.checkValidationHandler(event.target.value, this.state.controls[controlName].validation),
                 touched: true
-
-            }
-        };
+            })
+        });
         this.setState({controls: updatedControls});
     };
 
